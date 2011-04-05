828b266e87de073f1fb88cbf386b36b14e7c0844
edwin.bratini@gmail.com <edwin.bratini@gmail.com@56a0982c-ec2c-ac01-9902-17bb2d0e30d9>
2011-04-05 00:58:45 +0000

    first import

---
+++ Syscafil/src/com/prebea/syscafil/business/PlanManager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.prebea.syscafil.business;
+
+/**
+ *
+ * @author Edwin Bratini
+ */
+public class PlanManager {
+
+}
